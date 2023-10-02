241c25ad966b1375adcee447eb96b8b4ef955d3c
Pack <ppack254@gmail.com>
2023-10-03 11:50:12 +1300

    improve pagination display and sorting order

--- src/extensions/AccordionPageExtension.php
+++ src/extensions/AccordionPageExtension.php
@@ -1,71 +1,175 @@
 <?php
 
-namespace NinjaUnicorns\WysiwygAccordion\Extensions;
+namespace NinjaUnicorns\WysiwygAccordion\Models;
 
-use SilverStripe\ORM\DataExtension;
-use SilverStripe\Forms\FieldList;
-use SilverStripe\View\Requirements;
-use SilverStripe\Core\Config\Config;
+use SilverStripe\ORM\DataObject;
 use SilverStripe\Forms\LiteralField;
-use SilverStripe\Forms\GridField\GridFieldConfig_RecordEditor;
-use Symbiote\GridFieldExtensions\GridFieldOrderableRows;
-use SilverStripe\Lumberjack\Forms\GridFieldSiteTreeState;
-use SilverStripe\Forms\GridField\GridField;
-use NinjaUnicorns\WysiwygAccordion\Models\AccordionItem;
+use SilverStripe\ORM\DB;
+use SilverStripe\Versioned\Versioned;
+use Page;
 
 /**
- * Class AccordionPageExtension
+ * Class AccordionItem
  *
- * @property Page|AccordionPageExtension $owner
- * @method DataList|AccordionItem[] AccordionItems()
+ * There are no permissions, as they're part of Page, and not a separate thing.
+ *
+ * @property string $Title
+ * @property string $Content
+ * @property int $AccordionSet
+ * @property int $SortOrder
+ * @property int $PageID
+ * @method Page Page()
  */
-class AccordionPageExtension extends DataExtension
+class AccordionItem extends DataObject
 {
-    CONST NUMBER_OF_ITEMS_PER_PAGE = 20;
+    private static $table_name = 'AccordionItem';
 
-    private static $helpDescription = '<p><h4>Note</h4>Create Accordion items here, 
-    the title will be the header and the content will show on click.<br />To add the
-     accordion in the content, put <strong>[accordion,id=1]</strong> in the WYSIWYG 
-     editor at the place where you want the accordion to appear.<br /><u>Replace
-      the 1 with the actual set you want to render.</u></p>';
-
-    private static $has_many = [
-        'AccordionItems' => AccordionItem::class
+    private static $db = [
+        'Title'        => 'Varchar(255)',
+        'Content'      => 'HTMLText',
+        'AccordionSet' => 'Int(1)',
+        'SortOrder'    => 'Int',
     ];
 
-    public function updateCMSFields(FieldList $fields)
+    private static $has_one = [
+        'Page' => Page::class
+    ];
+
+    /**
+     * @var array
+     */
+    private static $summary_fields = [
+        'Title'        => 'Title',
+        'AccordionSet' => 'Accordion ID',
+    ];
+
+    private static $default_sort = 'SortOrder ASC';
+
+    /**
+     * @return FieldList
+     */
+    public function getCMSFields()
     {
-        if (class_exists('GridFieldSiteTreeState')) {
-            Requirements::css(LUMBERJACK_DIR . "/css/lumberjack.css");
+        $fields = parent::getCMSFields();
+        $fields->addFieldToTab('Root.Main', LiteralField::create('message', '<h5></h5>'), 'Title');
+        /** @var $accordion NumericField */
+        $accordion = $fields->fieldByName('Root.Main.AccordionSet');
+        $description = 'All items that share the same accordion have to have
+         the same accordion ID, accordion shortcode is [accordion,id=ID]
+         <br /><br /><strong>Important:</strong> Do not attempt to add an
+          accordionset with the same ID as this accordion in the content.';
+        $accordionIds = $this->Page()->AccordionItems()->column('AccordionSet');
+
+        if (count($accordionIds) > 0) {
+            $accordionIds = array_unique($accordionIds);
+            sort($accordionIds, SORT_NUMERIC);
+
+            $description .= '<br /><br />This page currently has following accordion IDs: ' . implode(
+                    ', ',
+                    $accordionIds
+                );
         }
 
-        $blacklistedPages = Config::inst()->get(self::class, 'PageBlacklist') ?: [];
-        if (!count($blacklistedPages) || !in_array($this->owner->ClassName, $blacklistedPages, true)) {
-            $helptext = _t('Accordion.HELP', self::$helpDescription);
-            $helpField = LiteralField::create('Help', $helptext);
-            $gridFieldConfig = GridFieldConfig_RecordEditor::create();
-            $gridFieldConfig->addComponent(new GridFieldOrderableRows('SortOrder'));
-            if (class_exists('GridFieldSiteTreeState')) {
-                $gridFieldConfig->addComponent(new GridFieldSiteTreeState());
-            }
-            $accordionGrid = GridField::create(
-                'AccordionItems',
-                'AccordionItems',
-                $this->owner->AccordionItems(),
-                $gridFieldConfig
-            );
-            $gridFieldConfig->removeComponentsByType('GridFieldAddExistingAutocompleter');
-            $fields->addFieldsToTab(
-                'Root.Accordion',
-                [
-                    $helpField,
-                    $accordionGrid
-                ]
-            );
+        $accordion->setDescription($description);
 
-            $gridFieldConfig
-                ->getComponentByType(GridFieldPage::class)
-                ->setItemsPerPage(self::NUMBER_OF_ITEMS_PER_PAGE); // Change 20 to your desired number
+        if ($this->ID == 0) {
+            $accordion->setValue(1);
         }
+
+        $fields->removeByName(['PageID', 'SortOrder', 'AccordionSet']);
+        $fields->addFieldToTab('Root.Main', $accordion, 'Content');
+
+        return $fields;
+    }
+
+    public function canDelete($member = null)
+    {
+        $can = parent::canDelete($member);
+        if ($this->wasPublished()) {
+            return false;
+        }
+
+        return $can;
+    }
+
+    public function wasPublished()
+    {
+        return DB::prepared_query('SELECT "ID" FROM "AccordionItem_Live" WHERE "ID" = ?', array($this->ID))->value()
+            ? true
+            : false;
+    }
+
+    public function onAfterDelete()
+    {
+        parent::onAfterDelete();
+        if ($this->isPublished()) {
+            $this->deleteFromStage('Live');
+        }
+    }
+
+    public function onBeforeWrite()
+    {
+        $this->handleNewItemSorting();
+
+        parent::onBeforeWrite();
+    }
+
+    /**
+     * Check if this page has been published.
+     *
+     * @return bool
+     */
+    public function isPublished()
+    {
+        if ($this->isNew()) {
+            return false;
+        }
+
+        $isPublished = DB::prepared_query(
+            'SELECT "ID" FROM "AccordionItem_Live" WHERE "ID" = ?',
+            array($this->ID)
+        )->value();
+        if ($isPublished) {
+            return Versioned::get_latest_version(static::class, $this->ID)->WasPublished;
+        }
+
+        return $isPublished;
+    }
+
+    /**
+     * Check if this page is new - that is, if it has yet to have been written to the database.
+     *
+     * @return bool
+     */
+    public function isNew()
+    {
+        /**
+         * This check was a problem for a self-hosted site, and may indicate a bug in the interpreter on their server,
+         * or a bug here. Changing the condition from empty($this->ID) to !$this->ID && !$this->record['ID'] fixed this.
+         */
+        if (empty($this->ID)) {
+            return true;
+        }
+
+        if (is_numeric($this->ID)) {
+            return false;
+        }
+
+        return stripos($this->ID, 'new') === 0;
+    }
+
+    private function handleNewItemSorting()
+    {
+        if ($this->SortOrder) {
+            return;
+        }
+
+        $items = $this->Page()->AccordionItems();
+
+        if (!($items instanceof DataList)) {
+            return;
+        }
+
+        $this->SortOrder = $items->max('Sort') + 1;
     }
 }
